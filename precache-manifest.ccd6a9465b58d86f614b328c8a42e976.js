894b0f9ee1057df4a29b7b1c14268bf8ef06614c
Jack <Jack@Jacks-MacBook-Pro.local>
2020-09-23 12:09:09 +0100

    Updates

---
+++ precache-manifest.ccd6a9465b58d86f614b328c8a42e976.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a97e31f344452b6ef5d08e0becea5d2",
+    "url": "/taskapp/index.html"
+  },
+  {
+    "revision": "5a984257908f65270b5f",
+    "url": "/taskapp/static/css/main.52fa7ea2.chunk.css"
+  },
+  {
+    "revision": "3671d8a5ef1537651e55",
+    "url": "/taskapp/static/js/2.adb43fb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/taskapp/static/js/2.adb43fb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a984257908f65270b5f",
+    "url": "/taskapp/static/js/main.6c3e6868.chunk.js"
+  },
+  {
+    "revision": "e169ce81c201c832a839",
+    "url": "/taskapp/static/js/runtime-main.5d2c84a5.js"
+  },
+  {
+    "revision": "f9ee2b9a8832d66c5d6e1c70f6fe3918",
+    "url": "/taskapp/static/media/done-1.f9ee2b9a.png"
+  }
+]);
